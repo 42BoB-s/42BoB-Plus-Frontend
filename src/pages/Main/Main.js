df9f2e5777c83955c76d6b79360b1a91e7697135
TonyHan <ourclub7279@gmail.com>
2021-08-28 23:52:13 +0900

    add: Main Header, font

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -2,7 +2,25 @@ import React from 'react';
 import './Main.scss';
 
 const Main = () => {
-  return <div>메인</div>;
+  return (
+    <header>
+      <div className="inner">
+        <a href="/" className="logo">
+          <img src="images/42bobs_logo.png" alt="42BoBs" />
+        </a>
+        <div className="sub-menu">
+          <ul className="menu">
+            <li className="mypage">
+              <a href="/">chahan</a>
+            </li>
+            <li className="chatting">
+              <a href="/">chatting</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Main;
