d68cf4c2c2a3db1c7ae22084ddb0f320e82d7705
TonyHan <ourclub7279@gmail.com>
2021-08-31 14:13:35 +0900

    Feat: 내 밥 친구 목록 크기 및 scss 임시 설정

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -3,7 +3,16 @@ import React from 'react';
 import './Main.scss';
 
 const Main = () => {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <div className="inner">
+        <div className="friend">
+          <div className="title">내 밥 친구 목록</div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Main;
