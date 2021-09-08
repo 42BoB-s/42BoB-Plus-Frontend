1e19fb7052c037098adedfcfe7f5389db74042ba
sham <sham@shamui-MacBookPro.local>
2021-09-08 16:49:02 +0900

    Feat : 예외 처리 시 alert 대신 새로운 컴포넌트(모달창) 생성

--- src/components/Bann/Bann.js
+++ src/components/Bann/Bann.js
@@ -1,96 +1,7 @@
-import { React, useState } from 'react';
+import { React, useState, useEffect } from 'react';
 import Modal from './Modal';
 import BannBox from './BannBox';
 import './Bann.scss';
-
-// 여러 개의 컴포넌트로 분할
-/*
-const Modal = ({ close, id, bann, message, type }) => {
-  // 열기, 닫기, 모달 헤더 텍스트를 부모로부터 받아옴
-  let typeEvent;
-  let text;
-
-  const BannEvent = () => {
-    // if 해당 아이디를 찾는다 - 존재하면 벤 추가해서 post, 존재하지 않으면 존재하지 않는다.
-    // 차단했다면 아이디를 전달해야한다!
-    bann(prevBann => [...prevBann, id]);
-    close();
-  };
-  const CancelEvent = () => {
-    bann(prevBann => prevBann.filter(e => e !== id));
-    close();
-  };
-
-  // type에 따라 차단 함수 차단 해제 함수가 분기된다.
-  if (type === "bann") {
-    typeEvent = BannEvent;
-    text = "차단";
-  } else {
-    typeEvent = CancelEvent;
-    text = "해제";
-  }
-  const BannNo = () => {
-    // 그냥 취소
-    close();
-  };
-
-  return (
-    // 모달이 열릴때 openModal 클래스가 생성된다.
-    <div className="modal">
-      <section>
-        <header>{text}</header>
-        <main>
-          <text className="id">{id}</text>
-          {message}
-        </main>
-        <footer>
-          <button type="button" className="yes" onClick={typeEvent}>
-            {text}
-          </button>
-          <button type="button" className="close" onClick={BannNo}>
-            취소
-          </button>
-        </footer>
-      </section>
-    </div>
-  );
-};
-
-const BannBox = ({ FindCadet, changeInput, input, bannCadet, cancelBann }) => {
-  return (
-    <div className="bann">
-      <div className="head">
-        <SearchIcon />
-
-        <form onSubmit={FindCadet}>
-          <input
-            onChange={changeInput}
-            type="text"
-            maxLength="12"
-            className="search-user"
-            value={input}
-          />
-        </form>
-      </div>
-      <div className="body">
-        {bannCadet.map(name => (
-          <div className="banned">
-            <text className="banned-id">{name}</text>
-            <button
-              type="button"
-              className="banned-cancel"
-              value={name}
-              onClick={cancelBann}
-            >
-              X
-            </button>
-          </div>
-        ))}
-      </div>
-    </div>
-  );
-};
-*/
 
 const Bann = () => {
   const [input, setInput] = useState('');
@@ -98,9 +9,8 @@ const Bann = () => {
   const [bannCadet, setBannCadet] = useState([]);
   const [bannModal, setBannModal] = useState(false);
   const [cancelModal, setCancelModal] = useState(false);
-
+  const [token, setToken] = useState();
   const url = 'https://api.intra.42.fr/oauth/token';
-  let token;
   const query =
     '?' +
     'grant_type=client_credentials' +
@@ -117,68 +27,48 @@ const Bann = () => {
     'scope=public';
   const bearer = 'Bearer ';
 
-  const getToken = res => {
-    console.log(res);
-    token = res.access_token;
-  };
-
-  fetch(url + query, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded',
-      'X-Mobile': 'false',
-      'response-Type': 'text',
-    },
-  })
-    .then(res => res.json())
-    .then(res => getToken(res));
-
-  const getUser = id => {
-    fetch(`https://api.intra.42.fr/v2/users/${id}`, {
-      method: 'GET',
+  useEffect(async () => {
+    const fetchToken = await fetch(url + query, {
+      method: 'POST',
       headers: {
-        'Content-Type': 'application/json',
-        Authorization: bearer + token,
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'X-Mobile': 'false',
         'response-Type': 'text',
       },
-    })
-      .then(res => res.json())
-      .then(res => console.log(res))
-      .catch(() => {
-        return 0;
-      });
-    return 1;
-  };
-  /*
-  const END_POINT_42_API = 'https://api.intra.42.fr';
-
-  const data = {
-    grant_type: 'client_credentials',
-    client_id:
-      '1cfa80163094e499e10108bfa2adb76409f5daec2084c00ce81a9c3a38416deb',
-    client_secret:
-      '1a3c7803cac7d2019618214318b6000842ce4983f37ac958407b5b3d50817422',
-  };
-  try {
-    const res = fetch(END_POINT_42_API + '/oauth/token', {
-      method: 'POST',
-      mode: 'cors',
-      headers: { 'Content-Type': 'application/json' },
-      data: JSON.stringify(data),
     });
-    if (res) {
-      return res.data;
+    const response = await fetchToken.json();
+    setToken(response.access_token);
+    // console.log(response.access_token);
+  }, []);
+
+  const getUser = async id => {
+    try {
+      const fetchId = await fetch(`https://api.intra.42.fr/v2/users/${id}`, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: bearer + token,
+          'response-Type': 'text',
+        },
+      });
+      if (fetchId.ok === false) {
+        return 'error'; // throw로 catch로 넘겨줄 수도 없기에 리턴받는 값이 if문에서 걸러지게 error를 전송.
+      }
+      const result = await fetchId.json();
+      // console.log(result);
+      return result.login;
+    } catch (e) {
+      // 사용자 문제가 아닌 자바스크립트 내부에서 에러가 발생했을 때 catch 사용?
+      return e;
     }
-  } catch (err) {
-    console.log(err);
-    throw err;
-  }
-*/
+  };
+
   const changeInput = e => {
     if (!bannModal) {
       setInput(e.currentTarget.value.replace(/[^A-Za-z]/gi, '')); // 영어만 입력되게끔
     }
   };
+
   const FindCadet = e => {
     e.preventDefault();
     if (!bannModal && input) {
@@ -205,6 +95,7 @@ const Bann = () => {
       <div className="bann-box">
         <text className="title">차단 목록</text>
         <BannBox
+          key="bannbox"
           FindCadet={FindCadet}
           changeInput={changeInput}
           input={input}
@@ -221,7 +112,7 @@ const Bann = () => {
           bannList={bannCadet}
           message="님을 차단하시겠습니까?"
           type="bann"
-          getUser={() => getUser(tempId)}
+          getUser={getUser}
         />
       )}
       {cancelModal && (
--- src/components/Bann/Modal.js
+++ src/components/Bann/Modal.js
@@ -1,7 +1,9 @@
-import { React } from 'react';
+import { React, memo, useState } from 'react';
 
-const Modal = ({ close, id, bann, bannList, message, type, getUser }) => {
+const Modal = memo(({ close, id, bann, bannList, message, type, getUser }) => {
   // 열기, 닫기, 모달 헤더 텍스트를 부모로부터 받아옴
+  const [wrong, setWrong] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
   let typeEvent;
   let text;
 
@@ -10,21 +12,31 @@ const Modal = ({ close, id, bann, bannList, message, type, getUser }) => {
     // 차단했다면 아이디를 전달해야한다!
     // 이미 차단한 경우에도 막아야만 한다.
     if (bannList.indexOf(id) !== -1) {
-      alert('중복된 아이디입니다!');
-      close();
-    } else if (getUser()) {
-      bann(prevBann => [...prevBann, id]);
+      setWrong(true);
+      setErrorMessage('중복된 아이디입니다!');
     } else {
-      alert('잘못된 아이디입니다!');
+      getUser(id) // getUser는 async 함수로 선언되었기에 promise의 리턴형인 resolve(성공시 호출), reject(에러 시 호출)로 리턴이 된다. 그에 맞춰 우리는 then으로 resolve를, catch로 reject를 받는다.
+        .then(userId => {
+          if (userId === 'error') {
+            setWrong(true);
+            setErrorMessage('존재하지 않는 사용자입니다!');
+          } else {
+            bann(prevBann => [...prevBann, userId]);
+            close();
+          }
+        })
+        .catch(error => {
+          console.log(error);
+          alert(error);
+        });
     }
-    close();
   };
+
   const CancelEvent = () => {
     bann(prevBann => prevBann.filter(e => e !== id));
     close();
   };
 
-  // type에 따라 차단 함수 차단 해제 함수가 분기된다.
   if (type === 'bann') {
     typeEvent = BannEvent;
     text = '차단';
@@ -32,31 +44,53 @@ const Modal = ({ close, id, bann, bannList, message, type, getUser }) => {
     typeEvent = CancelEvent;
     text = '해제';
   }
+
+  // type에 따라 차단 함수 차단 해제 함수가 분기된다.
   const BannNo = () => {
     // 그냥 취소
     close();
   };
 
+  const closeWrong = () => {
+    setWrong(false);
+    close();
+  };
+
   return (
     // 모달이 열릴때 openModal 클래스가 생성된다.
-    <div className="modal">
-      <section>
-        <header>{text}</header>
-        <main>
-          <text className="id">{id}</text>
-          {message}
-        </main>
-        <footer>
-          <button type="button" className="yes" onClick={typeEvent}>
-            {text}
-          </button>
-          <button type="button" className="close" onClick={BannNo}>
-            취소
-          </button>
-        </footer>
-      </section>
-    </div>
+    <>
+      <div className="modal">
+        <section>
+          <header>{text}</header>
+          <main>
+            <text className="id">{id}</text>
+            {message}
+          </main>
+          <footer>
+            <button type="button" className="yes" onClick={typeEvent}>
+              {text}
+            </button>
+            <button type="button" className="close" onClick={BannNo}>
+              취소
+            </button>
+          </footer>
+        </section>
+      </div>
+      {wrong && (
+        <div className="small-modal">
+          <section>
+            <header>에러</header>
+            <main>{errorMessage}</main>
+            <footer>
+              <button type="button" className="yes" onClick={closeWrong}>
+                닫기
+              </button>
+            </footer>
+          </section>
+        </div>
+      )}
+    </>
   );
-};
+});
 
 export default Modal;
