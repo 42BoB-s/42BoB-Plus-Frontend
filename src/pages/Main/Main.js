d98ab608ec8e5f3d25b02608039ecee3c451ae12
choekko <choekko@gmail.com>
2021-09-30 01:00:21 +0900

    test: 메인 페이지 무한 스크롤 테스트

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,13 +1,44 @@
 import Header from 'components/Header';
-import React, { useRef } from 'react';
+import React, { useRef, useState, useCallback } from 'react';
 import './Main.scss';
+import TestCard from 'components/testCard';
+import getRoomList from 'apis/getRoomList';
+import useIntersectionObserver from 'utils/hooks/useIntersectionObserver';
 
 const Main = () => {
+  const [currPageIndex, setCurrPageIndex] = useState(1);
+  const [roomList, setLoomList] = useState([]);
+
+  const fetchRoomList = async pageIndex => {
+    const response = await getRoomList(pageIndex);
+    if (response) {
+      setLoomList(prev => [...prev, ...response]);
+    }
+  };
+
+  const handleIntersect = useCallback(async () => {
+    await fetchRoomList(currPageIndex);
+    setCurrPageIndex(preState => preState + 1);
+  }, [currPageIndex]);
+
+  const footerRef = useRef();
+
+  const [target, setTarget] = useIntersectionObserver({
+    onIntersect: handleIntersect,
+    targetElement: footerRef,
+    options: { rootMargin: '10px' },
+    changeDetection: currPageIndex,
+  });
+
+  console.log(target, setTarget);
+
   const nameInputRef = useRef();
 
   const handleClick = () => {
     sessionStorage.setItem('username', nameInputRef.current.value);
   };
+
+  console.log(currPageIndex, setCurrPageIndex, roomList);
 
   return (
     <>
@@ -21,6 +52,23 @@ const Main = () => {
       <button aria-label="saveName" type="button" onClick={handleClick}>
         임시저장
       </button>
+
+      {roomList.map(e => {
+        return (
+          <TestCard
+            roomId={e.roomId}
+            title={e.title}
+            menus={e.menus}
+            meetTime={e.meetTime}
+            location={e.location}
+            capacity={e.capacity}
+            owner={e.owner}
+            participants={e.participants}
+            status={e.status}
+          />
+        );
+      })}
+      <footer ref={footerRef} />
     </>
   );
 };
