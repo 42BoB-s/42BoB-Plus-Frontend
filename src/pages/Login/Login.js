b9e595accc316578dce7aa6a7906dfd8081ba1fe
choekko <choekko@gmail.com>
2021-10-30 18:21:32 +0900

    feat: 임시 로그인 페이지 구현

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -2,7 +2,25 @@ import React from 'react';
 import './Login.scss';
 
 const Login = () => {
-  return <div>로그인</div>;
+  // TODO : 기다리는 사람 명수 끌어와야 함
+  const numberOfPeopleWating = 5;
+  return (
+    <div className="login__wrap">
+      <img
+        src="/assets/bobs_logo.png"
+        alt="bobs_logo"
+        className="login__logo"
+      />
+      <p className="login__description">
+        지금{' '}
+        <span style={{ fontWeight: 'bold' }}>{numberOfPeopleWating}명</span>이
+        밥 친구를 찾고 있어요!
+      </p>
+      <button type="button" className="login__button">
+        로그인
+      </button>
+    </div>
+  );
 };
 
 export default Login;
