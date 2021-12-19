c8fcc93366567785047f8449df9576d746dd0a11
Sangjoon Ham <gulsamcono@gmail.com>
2021-12-19 20:23:54 +0900

    Feat : 마이페이지 getStat API 추가.

--- src/pages/MyPage/MyPage.js
+++ src/pages/MyPage/MyPage.js
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Profile from 'components/Profile';
 import MealStat from 'components/MealStat';
 import MealLog from 'components/MealLog';
 import Bann from 'components/Bann';
+import axios from 'axios';
+import getStat from 'apis/getStat';
 
 import './MyPage.scss';
 
 const MyPage = () => {
+  useEffect(async () => {
+    const userInfo = await getStat();
+    console.log(userInfo.data);
+  }, []);
   return (
     <div className="mypage">
       <></>
