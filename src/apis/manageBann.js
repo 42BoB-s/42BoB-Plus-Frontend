892d5c3073474235fd54b3b27d41f438cf5e7dd1
GulSam00 <gulsamcono@gmail.com>
2022-02-13 20:24:54 +0900

    Feat : 벤 등록 삭제 API 붙이기. 백엔드 수정될 예정.

--- src/apis/manageBann.js
+++ src/apis/manageBann.js
@@ -1,18 +1,19 @@
 import axios from 'axios';
 import END_POINT from 'apis/END_POINT';
-// import getSavedJWT from 'utils/getSavedJWT';
+import getSavedJWT from 'utils/getSavedJWT';
 
 const manageBann = async (id, method) => {
   const url = END_POINT + `/bobs/mypage/ban/dev`;
   const config = {
-    // headers: {
-    //   Authorization: getSavedJWT(),
-    // },
-    name: id,
+    headers: {
+      Authorization: getSavedJWT(),
+    },
   };
+  const body = { name: id };
   let response;
-  if (method === 'patch') response = await axios.patch(url, config);
-  else if (method === 'delete') response = await axios.delete(url, config);
+  if (method === 'patch') response = await axios.patch(url, body, config);
+  else if (method === 'delete')
+    response = await axios.delete(url, body, config);
   else throw Error('wrong method');
   return response;
 };
